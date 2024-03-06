570437c45f76d72b2377a7c082c10b6c718b2f91
testcodeuser <test@yahoo.com>
2024-03-06 20:01:10 +0530

    final version of webapp

--- src/app.py
+++ src/app.py
@@ -1,21 +1,17 @@
 from flask import Flask, jsonify, render_template
 import requests
-import time
 
 app = Flask(__name__)
-public_ip_buffer = None
 
 def get_public_ip():
-    global public_ip_buffer
     try:
         response = requests.get('https://api.ipify.org')
         if response.status_code == 200:
-            public_ip_buffer = response.text
-            return public_ip_buffer
+            return response.text
         else:
             return "Failed to retrieve public IP"
     except Exception as e:
-        return "An error occurred: {e}"
+        return f"An error occurred: {e}"
 
 @app.route('/get_public_ip')
 def get_public_ip_route():
@@ -24,11 +20,8 @@ def get_public_ip_route():
 
 @app.route('/reversed_public_ip')
 def reversed_public_ip_route():
-    global public_ip_buffer
-    if public_ip_buffer is None:
-        get_public_ip()  # Fetch public IP if buffer is empty
-    time.sleep(2)
-    reverse_public_ip = public_ip_buffer[::-1]
+    public_ip = get_public_ip()
+    reverse_public_ip = '.'.join(public_ip.split('.')[::-1]) # Corrected variable name
     return jsonify({'rIPv4': reverse_public_ip})
 
 @app.route('/')
