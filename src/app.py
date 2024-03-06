78de2e2819284dd467a15f200700ae053146ad9b
testcodeuser <test@yahoo.com>
2024-03-06 17:20:15 +0530

    Flask Application developed

---
+++ src/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, render_template
+import requests
+
+app = Flask(__name__)
+
+def get_public_ip():
+    try:
+        response = requests.get('https://api.ipify.org')
+        if response.status_code == 200:
+            return response.text
+        else:
+            return "Failed to retrieve public IP"
+    except Exception as e:
+        return f"An error occurred: {e}"
+
+@app.route('/get_public_ip')
+def get_public_ip_route():
+    public_ip = get_public_ip()
+    return jsonify({'ip': public_ip})
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
